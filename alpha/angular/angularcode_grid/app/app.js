0207bbdfe39d100d806edc18f022c581b6afdb6f
Phong <thanhphongtran24@gmail.com>
2015-04-03 21:24:32 -0700

    learning angular

--- alpha/angular/angularcode_grid/app/app.js
+++ alpha/angular/angularcode_grid/app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ui.bootstrap']);
+var app = angular.module('myApp', ['ui.bootstrap','uiTable']);
 
 app.filter('startFrom', function() {
     return function(input, start) {
@@ -7,7 +7,7 @@ app.filter('startFrom', function() {
             return input.slice(start);
         }
         return [];
-    }
+    };
 });
 app.controller('customersCrtl', function ($scope, $http, $timeout) {
     $http.get('ajax/getCustomers.php').success(function(data){
@@ -29,5 +29,59 @@ app.controller('customersCrtl', function ($scope, $http, $timeout) {
     $scope.sort_by = function(predicate) {
         $scope.predicate = predicate;
         $scope.reverse = !$scope.reverse;
+    };
+});
+
+app.controller('MyCtrl', function($scope) {
+    $scope.tableOptions = {
+        // Use this on IE8 or very slow hardware.
+        // Your cells will not be data bound
+        // but use simple string replacement.
+        //noCellBinding: true,
+        // Use this to disable the dynamic row height feature
+        // (improves rendering performance).
+        //rowsHeight:24,
+        // use this to set the available with for all columns
+        // (useful for star-based layouts).
+        //columnsWidth:1600,
+        // Use headTemplate to define the column header cell template.
+        // Use dataTemplate to define the data cell template for this column.
+        columns:[
+            {
+                title:'First Name', field:'firstName',
+                sort:'', sortOrder:0,
+                width:'1*', fixed:'',
+                headTemplate:'', dataTemplate:''
+            },
+            {
+                title:'Last Name', field:'lastName',
+                sort:'', sortOrder:0,
+                width:'2*', fixed:'',
+                headTemplate:'', dataTemplate:''
+            },
+            {
+                title:'First Name', field:'firstName',
+                sort:'', sortOrder:0,
+                width:'50px', fixed:'',
+                headTemplate:'', dataTemplate:''
+            }
+        ],
+        // can be an array of row data or a function
+        // that receives the row index and
+        // returns the data for the requested row.
+        data:[
+            {firstName:"Walter", lastName:"Wurst", age:20},
+            {firstName:"Peter", lastName:"Pan", age:21},
+            {firstName:"Erwin", lastName:"Erster", age:22},
+            {firstName:"Paula", lastName:"Pummel", age:23}
+        ],
+        // in case data is a function,
+        // total has to indicate the total number of rows available.
+        //total:100,
+        // the selected rows data.
+        selection: [],
+        // allow the selection of multiple rows with the
+        // SHIFT and CTRL keys.
+        allowMultiSelection: true
     };
 });
