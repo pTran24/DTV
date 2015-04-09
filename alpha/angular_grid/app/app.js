3a0731af191941ebc55b6417ac5df41454719d52
Phong <thanhphongtran24@gmail.com>
2015-04-08 17:18:29 -0700

    changing

--- alpha/angular_grid/app/app.js
+++ alpha/angular_grid/app/app.js
@@ -9,16 +9,29 @@ app.filter('startFrom', function() {
         return [];
     }
 });
+app.filter('regex', function() {
+  return function(input, field, regex) {
+      var patt = new RegExp(regex);
+      var out = [];
+      for (var i = 0; i < input.length; i++){
+          if(patt.test(input[i][field]))
+              out.push(input[i]);
+      }
+    return out;
+  };
+});
 app.controller('customersCtrl', function ($scope, $http, $timeout) {
     $http.get('ajax/getCustomers.php').success(function(data){
-        $scope.list = data;
-        $scope.currentPage = 1; //current page
-        $scope.entryLimit = 5; //max no of items to display in a page
-        $scope.filteredItems = $scope.list.length; //Initially for no filter
+        $scope.list = data; // query result
+        $scope.currentPage = 1; // current page
+        $scope.entryLimit = 5; // max no of items to display in a page
+        $scope.filteredItems = $scope.list.length; // Initially for no filter
         $scope.totalItems = $scope.list.length;
+        $scope.setRange();
     });
     $scope.setPage = function(pageNo) {
         $scope.currentPage = pageNo;
+        $scope.setRange();
     };
     $scope.filter = function() {
         $timeout(function() {
@@ -29,4 +42,29 @@ app.controller('customersCtrl', function ($scope, $http, $timeout) {
         $scope.predicate = predicate;
         $scope.reverse = !$scope.reverse;
     };
+    $scope.setRange = function() {
+        console.log("called");
+        $scope.showBegin = $scope.currentPage * $scope.entryLimit - $scope.entryLimit + 1;
+        $scope.showEnd   = $scope.currentPage * $scope.entryLimit;
+        if ($scope.showEnd > $scope.filteredItems) {
+            $scope.showEnd = $scope.filteredItems;
+        }
+    };
+    $scope.tableHeader = [
+        'Environment',
+        'HostIP',
+        'Status',
+        'ServiceName',
+        'StartTime',
+        'Capacity',
+        'Txn',
+        'Errors',
+        'Build',
+        'Project',
+        'Author',
+        'ModifyDate',
+        'Port',
+        'BasePath'
+    ];
+    $scope.test = "Status";
 });
